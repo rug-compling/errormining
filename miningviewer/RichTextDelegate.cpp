567c886ac86f1bfbaebe7f4ad58fcfc7c0900b2c
Daniel de Kok <me@danieldk.eu>
2011-01-18 16:14:44 +0100

    Cleanups.

--- miningviewer/RichTextDelegate.cpp
+++ miningviewer/RichTextDelegate.cpp
@@ -27,18 +27,6 @@ void RichTextDelegate::paint(QPainter *painter,
     if (index.column() != d_column)
         return QStyledItemDelegate::paint(painter, option, index);
 
-    QStyleOptionViewItemV4 opt = option;
-    initStyleOption(&opt, index);
-
-    //painter->save();
-
-    /*QTextDocument doc;
-    QTextOption textOption = doc.defaultTextOption();
-    textOption.setWrapMode(QTextOption::NoWrap);
-    doc.setDefaultTextOption(textOption);
-    doc.setDocumentMargin(0);
-    doc.setHtml(opt.text);*/
-
     bool selected = option.state & QStyle::State_Selected;
     QPalette palette(option.palette);
     palette.setColor(QPalette::Active,
@@ -53,27 +41,19 @@ void RichTextDelegate::paint(QPainter *painter,
     QRect labelRect(option.rect);
     d_label->setFixedSize(qMax(0, labelRect.width()), labelRect.height());
     d_label->setPalette(palette);
-    d_label->setText(opt.text);
+
+    QString text = index.model()->data(index).toString();
+    d_label->setText(text);
 
     QPixmap pixmap(d_label->size());
 
-    QString cacheKey = QString("LABEL:%1.%2.%3x%4").arg(selected).arg(opt.text).arg(labelRect.width()).arg(labelRect.height());
+    QString cacheKey = QString("LABEL:%1.%2.%3x%4").arg(selected).arg(text).arg(labelRect.width()).arg(labelRect.height());
     if (!QPixmapCache::find(cacheKey, &pixmap)) {
         d_label->render(&pixmap);
         QPixmapCache::insert(cacheKey, pixmap);
     }
 
     painter->drawPixmap(labelRect, pixmap);
-
-    //opt.text = "";
-    //opt.widget->style()->drawControl(QStyle::CE_ItemViewItem, &opt, painter);
-
-
-    //painter->translate(opt.rect.left(), opt.rect.top());
-    //QRect clip(0, 0, opt.rect.width(), opt.rect.height());
-    //doc.drawContents(painter, clip);
-
-    //painter->restore();
 }
 
 QSize RichTextDelegate::sizeHint(QStyleOptionViewItem const &option,
@@ -82,12 +62,11 @@ QSize RichTextDelegate::sizeHint(QStyleOptionViewItem const &option,
     if (index.column() != d_column)
         QStyledItemDelegate::sizeHint(option, index);
 
-    QStyleOptionViewItemV4 opt = option;
-    initStyleOption(&opt, index);
+    QString text = index.model()->data(index).toString();
 
     QTextDocument doc;
     doc.setDefaultFont(option.font);
-    doc.setHtml(opt.text);
+    doc.setHtml(text);
 
     return QSize(doc.idealWidth(), option.fontMetrics.height());
 }
