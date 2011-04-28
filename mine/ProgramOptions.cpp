8650323f3c28732086c73a9eb5ec73998318c62a
Daniël de Kok <me@danieldk.eu>
2011-04-28 10:42:45 +0200

    Bring back the '-m' flag.
    (What happened?)

--- mine/ProgramOptions.cpp
+++ mine/ProgramOptions.cpp
@@ -1,7 +1,7 @@
 #include "ProgramOptions.ih"
 
 ProgramOptions::ProgramOptions(int argc, char *argv[])
-	: d_n(1), d_ngramExpansion(true), d_expansionFactorAlpha(1.0),
+	: d_n(1), d_m(1), d_ngramExpansion(true), d_expansionFactorAlpha(1.0),
 	d_frequency(2), d_smoothing(false), d_smoothingBeta(0.1),
 	d_sortAlgorithm(SuffixArray<int>::SSORT), d_suspFrequency(0),
 	d_suspThreshold(0.001), d_threshold(0.001), d_verbose(true),
@@ -13,7 +13,7 @@ ProgramOptions::ProgramOptions(int argc, char *argv[])
 	opterr = 0;
 
 	int opt;
-	while ((opt = getopt(argc, argv, "b:ce:f:n:o:qs:t:u")) != -1)
+	while ((opt = getopt(argc, argv, "b:ce:f:m:n:o:qs:t:u")) != -1)
 	{
 		switch (opt)
 		{
@@ -29,6 +29,9 @@ ProgramOptions::ProgramOptions(int argc, char *argv[])
 			break;
 		case 'f':
 			d_frequency = parseString<size_t>(optarg);
+			break;
+		case 'm':
+			d_m = parseString<size_t>(optarg);
 			break;
 		case 'n':
 			d_n = parseString<size_t>(optarg);
